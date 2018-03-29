cf5da56109c0763a57eb742a2ec22c68a4f3850f
Magda Gutowska <m.gutowska0909@gmail.com>
2018-03-29 16:35:51 +0200

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// scripts.js
+
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+var allNames = femaleNames.contact(maleNames);
+
+consol.log(allNames);
+
+var newName = "Marian";
+var indexOf(newName) = allNames.indexOf("Marian");
+
+consol.log(allNames[indexOf(newName)]);
+
+if ( allNames.indexOf(Marian) === -1) {
+	console.log('Ania występuje w tablicy pod indexem ' + allNames.indexOf(Marian));
+}
+else ( allNames.push(newName)) {
+	console.log(allNames);
+}
